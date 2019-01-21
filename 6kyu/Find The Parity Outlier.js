88f11b6aed1c38b95cd703bd3bb7e7a8960f26f0
Paul <gasior002@gmail.com>
2019-01-21 01:24:40 +0100

    Find The Parity Outlier

---
+++ 6kyu/Find The Parity Outlier.js
@@ -0,0 +1,20 @@
+function findOutlier(integers) {
+  var oddNumbers = integers.filter((e) => e % 2 != 0);
+  return (oddNumbers.length === 1) ? parseInt(oddNumbers, 10) : parseInt(integers.filter((e) => e % 2 == 0), 10);
+}
+
+var result = findOutlier([2, 3, 8, 12, 20]);
+
+console.log(result);
+
+// other wersion without unnesesary .parseInt()
+/*
+function findOutlier(integers) {
+  var oddNumbers = integers.filter((e) => e % 2 != 0);
+  return (oddNumbers.length === 1) ? oddNumbers[0] : integers.filter((e) => e % 2 == 0)[0];
+}
+
+var result = findOutlier([2, 3, 8, 12, 20]);
+
+console.log(result);
+*/
